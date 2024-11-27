d0a722d5546b14a1d4e60972a26cb807f7cc7aca
Harsh <haryan998@gmail.com>
2024-11-27 22:24:53 +0530

    Made the Side bar.

---
+++ src/Componenets/Shop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Shop() {
+  return <h1>Welcome to the Shop Page</h1>;
+}
+
+export default Shop;
---
+++ src/Componenets/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faStore,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h3>Plushie</h3>
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={faHome} />
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faStore} />
+          <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faShoppingCart} />
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+      <ul className="link-bottom">
+        <li>
+          <Link to="/help">Help</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
