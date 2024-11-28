46ec6df0ab5652a48dd269cd03bc04a67a8725fd
Harsh <haryan998@gmail.com>
2024-11-28 22:03:34 +0530

    Home page done.

--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  const images = [
+    "/src/Images/p1.jpg",
+    "/src/Images/p2.jpg",
+    "/src/Images/p3.jpg",
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "relative",
+        overflow: "hidden",
+        width: "100%",
+        height: "100%",
+        boxSizing: "border-box",
+      }}
+    >
+      <img
+        src={images[currentImageIndex]}
+        alt="slideshow"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          borderRadius: "20px",
+          brightness: "0.7",
+        }}
+      />
+    </div>
+  );
 }
 
 export default Home;
